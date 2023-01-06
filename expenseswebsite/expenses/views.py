9bfa91b835b960a9bab489f8179da87a0cf26b62
Collins07 <collinsabaya07@gmail.com>
2023-01-06 18:50:54 +0300

    configure urls

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'templates/index.html')
+
+def add_expenses(request):
+    return render(request, 'templates/add_expenses.html')    
